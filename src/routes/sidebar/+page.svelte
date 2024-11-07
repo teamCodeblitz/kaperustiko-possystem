a346a34cb99d31d37d6b836bf345bf9adae1b1a6
Michael <202211836@gordoncollege.edu.ph>
2024-11-08 02:15:02 +0800

    Initial Commit

--- src/routes/sidebar/+page.svelte
+++ src/routes/sidebar/+page.svelte
@@ -4,24 +4,30 @@
     import { faClipboardCheck, faExchangeAlt, faBoxOpen } from '@fortawesome/free-solid-svg-icons';
     import { onMount } from 'svelte';
     let isActive = false;
+    let activeIcon = ''; // Track the active icon
 
-    onMount(() => {
-        isActive = window.location.pathname === '/main-pos';
-    });
+
+    function setActive(icon: string) {
+        activeIcon = icon; // Set the active icon
+    }
 </script>
 
 <div class="flex flex-col h-screen bg-green-950 w-14 ">
     <div class="flex justify-center mt-4 mb-24">
         <img src="./icon.png" alt="Logo" class="w-10 h-10 " />
     </div>
-    <div class="flex items-center p-4 hover:bg-green-600 justify-center {isActive ? 'text-black' : 'text-white'} bg-yellow-300 rounded-md h-12 ml-1 mr-1">
-        <FontAwesomeIcon icon={faTachometerAlt} class="text-2xl" />
+    <div class="flex items-center p-4 hover:bg-green-600 justify-center">
+        <button type="button" on:click={() => { window.location.href = '/main-pos'; setActive('dashboard'); }} class="flex items-center">
+            <FontAwesomeIcon icon={faTachometerAlt} class={`text-2xl ${activeIcon === 'dashboard' ? 'text-black' : 'text-white'}`} />
+        </button>
     </div>
     <div class="flex items-center p-4 hover:bg-green-600 justify-center">
-        <FontAwesomeIcon icon={faClipboardCheck} class="text-white text-2xl" />
+        <button type="button" on:click={() => { window.location.href = '/sales-page'; setActive('sales'); }} class="flex items-center">
+            <FontAwesomeIcon icon={faClipboardCheck} class={`text-2xl ${activeIcon === 'sales' ? 'text-black' : 'text-white'}`} />
+        </button>
     </div>
     <div class="flex items-center p-4 hover:bg-green-600 justify-center">
-        <FontAwesomeIcon icon={faExchangeAlt} class="text-white text-2xl" />
+        <FontAwesomeIcon icon={faExchangeAlt} class={`text-2xl ${activeIcon === 'exchange' ? 'text-black' : 'text-white'}`} />
     </div>
     <div class="flex items-center p-4 hover:bg-green-600 justify-center">
         <FontAwesomeIcon icon={faBoxOpen} class="text-white text-2xl" />
